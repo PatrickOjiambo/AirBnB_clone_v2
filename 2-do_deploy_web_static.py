1289327a1985908ca868b021788f956bc1668374
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-10-05 23:36:39 +0300

    Number 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,13 +23,15 @@ def do_deploy(archive_path):
     if run("mkdir -p /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
-    if run("tar -xzvf /tmp/{} -C /data/web_static/releases/{}".format(file, name)).failed() is True:
+    if run("tar -xzvf /tmp/{} -C /data/web_static/"
+           "releases/{}".format(file, name)).failed() is True:
         return False
     if run("rm /tmp/{}".format(file)).failed() is True:
         return False
     if run("rm -rf /data/web_static/current").failed() is True:
         return False
-    if run("ln -sf /data/web_static/releases/{} /data/web_static/current".format(name)).failed() is True:
+    if run("ln -sf /data/web_static/releases/{}"
+           " /data/web_static/current".format(name)).failed() is True:
         return False
     if run("mv /data/web_static/releases/{}/web_static/* "
            "/data/web_static/releases/{}/".format(name, name)).failed is True:
